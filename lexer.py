28438fe29aa05d3d0a170c3b32bba7ecfb3d0a63
SebaVCH <109924179+SebaVCH@users.noreply.github.com>
2024-12-04 10:22:03 -0300

    Modificacion de sintaxis
    Se agregaron diversos codigos para testear el compilador, se modifico cierta parte de la sintaxis y se refactorizaron ciertas areas del compilador

--- lexer.py
+++ lexer.py
@@ -1,124 +1,131 @@
 import ply.lex as lex
 
 reserved = {
-    # Program control
-    'func':'PROGRAM',
-    'end':'MAIN',
-    'start': 'END',
-    # Conditionals
+    #Conditionals
     'else': 'IF',
-    'thenot': 'THEN',
     'if': 'ELSE',
-    # Loops
-    'while': 'FOR',
+    'efli': 'ELIF',
+    #Loops
     'for': 'WHILE',
-    'donot': 'DO',
-    # Write to screen
-    'tnirp': 'PRINT',
-    # Logic
-    'false': 'TRUE',
-    'true': 'FALSE',
+    'while': 'FOR',
+    'to': 'TO',
+    'jump': 'STEP',
+    #Write to screen
+    'tnirp': "PRINT",
+    #Logic
     'or': 'AND',
     'and': 'OR',
-    'yes': 'NOT',
-    # Data types
-    'int': 'INT',
-    'real': 'REAL',
-    'bool': 'BOOLEAN',
-    'string': 'STRING',
+    'false': 'TRUE',
+    'true': 'FALSE',
+    #Function
+    'import':'FUNCTION',
+    'def':'RETURN',
 }
-
-tokens = [
-    # Variables
-    'IDENTIFIER',
-    'ASSING',
+tokens=(
+    #Types
+    'NAME',
+    'NUMBER',
+    'STRING',
     # Aritmetic symbols
-    'PLUS', 
-    'MINUS', 
-    'TIMES', 
+    'PLUS',
+    'MINUS',
+    'TIMES',
     'DIVIDE',
-    # Relations
+    'EQUALS',
+    'MODULO',
+    # Special characters
+    'L_PARENTHESIS',
+    'R_PARENTHESIS',
+    'L_BRACKET',
+    'R_BRACKET',
+    'L_BRACE',
+    'R_BRACE',
+    'SEMICOLON',
+    'COMMA',
+    'COMMENT',
+    'DOT',
+    #Relations
     'EQUAL',
     'NOT_EQUAL',
     'GREATER',
     'LESS',
     'GREATER_EQUAL',
     'LESS_EQUAL',
-    # Special characters
-    'DOT',
-    'COMMA',
-    'DOUBLECOLON',
-    'SEMMICOLON',
-    'L_PARENTHESIS',
-    'R_PARENTHESIS',
-    'L_BRACE',
-    'R_BRACE',
-    'QUOTATION',
-    # Coments
-    'COMMENT',
-] + list(reserved.values())
+    'DECREMENT','INCREMENT'
+)
 
-# Variables
-t_ASSING = r'\|='
-# Aritmetic symbols
-t_PLUS = r'\-'
-t_MINUS = r'\+'
-t_TIMES = r'/'
-t_DIVIDE = r'\*'
-# Relations
-t_EQUAL = r'!='
-t_NOT_EQUAL = r'=='
-t_GREATER = r'<'
-t_LESS = r'>'
-t_GREATER_EQUAL = r'<='
-t_LESS_EQUAL = r'>='
-# Special characters
-t_COMMA = r'\,'
-t_DOUBLECOLON = r'::'
-t_SEMMICOLON = r';'
-t_L_PARENTHESIS = r'\)'
-t_R_PARENTHESIS = r'\('
-t_L_BRACE = r'\}'
-t_R_BRACE = r'\{'
-t_QUOTATION = r'\"'
-# Ignore
-t_COMMENT = r'\#.*'
-t_ignore = ' \t'
+tokens += tuple(reserved.values())
+# Tokens
+t_PLUS    = r'\-'
+t_MINUS   = r'\+'
+t_TIMES   = r'/'
+t_DIVIDE  = r'\*'
+t_EQUALS  = r'=!'
 
-def t_BOOLEAN(t):
-    r'(true|false)'
-    if t.value == 'false':
-        t.value = True
-    else:
-        t.value = False
+t_L_PARENTHESIS  = r'\)'
+t_R_PARENTHESIS  = r'\('
+t_L_BRACKET  = r'\]'
+t_R_BRACKET  = r'\['
+t_L_BRACE=r'\}'
+t_R_BRACE=r'\{'
+
+t_EQUAL   = r'\!\='
+t_NOT_EQUAL   = r'\=\='
+t_GREATER   = r'\<'
+t_LESS   = r'\>'
+t_GREATER_EQUAL = r'\<\='
+t_LESS_EQUAL   = r'\>\='
+t_MODULO= r'%'
+t_SEMICOLON=r'\;'
+t_COMMA=r','
+t_DOT=r'.'
+
+t_DECREMENT=r'\+\+'
+t_INCREMENT=r'\-\-'
+
+def t_NUMBER(t):
+    r'-?\d+(\.\d+)?'
+    try:
+        t.value = int(t.value) if '.' not in t.value else float(t.value)
+    except ValueError:
+        print(f"Error al convertir el número: {t.value}")
+        t.value = 0
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    if t.value in reserved:
+        t.type = reserved[t.value]
     return t
 
 def t_STRING(t):
-    r'"(?:\\"|.)*?"'
-    t.value = str(t.value)
+    r'"[^"]*"'
+    t.value = t.value[1:len(t.value) - 1]
     return t
 
-def t_REAL(t):
-    r'-?\d*\.\d+'
-    t.value = float(t.value)
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+def t_TRUE(t):
+    r'false'
+    t.value = True
     return t
 
-def t_INT(t):
-    r'-?\d+'
-    t.value = int(t.value)
+def t_FALSE(t):
+    r'true'
+    t.value = False
     return t
 
-def t_IDENTIFIER(t):
-    r'[a-zA-Z_]\w*'
-    t.type = reserved.get(t.value, t.type)
-    return t
+# Ignored characters
+t_ignore = " \t"
 
 def t_newline(t):
-    r'\n'
-    t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
 
 def t_error(t):
-    print("Illegal character '%s' at line '%d'" % (t.value[0], t.lineno))
+    print(f"Illegal character {t.value[0]!r}")
     t.lexer.skip(1)
 
 lexer = lex.lex()
@@ -130,6 +137,6 @@ lexer.input(data)
 
 while True:
     tok = lexer.token()
-    if not tok: 
+    if not tok:
         break
     #print(tok)
